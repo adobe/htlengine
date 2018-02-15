1a06c75493b2b9a45c4a40c6f8ac66795e9eb5ec
Tobias Bocanegra <tripod@adobe.com>
2018-02-15 12:45:05 +0900

    initial experiments for htl engine

---
+++ README.md
@@ -0,0 +1,14 @@
+# HTL Parser for Javascript
+
+## Build
+
+```bash
+$ yarn install
+$ yarn build
+```
+
+## run
+
+```bash
+$ node src/main.js tests/simple.html
+```
---
+++ src/main.js
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+const antlr4 = require('antlr4');
+
+const HTMLLexer = require('./generated/HTMLLexer').HTMLLexer;
+const HTMLParser = require('./generated/HTMLParser').HTMLParser;
+const HTMLParserListener = require('./generated/HTMLParserListener').HTMLParserListener;
+
+
+
+const filename = process.argv[2];
+const chars = new antlr4.FileStream(filename);
+const lexer = new HTMLLexer(chars);
+const tokens = new antlr4.CommonTokenStream(lexer);
+const parser = new HTMLParser(tokens);
+
+parser.buildParseTrees = true;
+
+const tree = parser.htmlDocument();
+
+console.log(tree.toStringTree());
+console.log(tree.getText());
+
