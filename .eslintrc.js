0376188063a8300b7efbac38307a48b48484a6eb
Tobias Bocanegra <tripod@adobe.com>
2020-05-12 10:32:19 +0900

    chore(lint): fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,31 +11,31 @@
  */
 
 module.exports = {
-  'env': {
-    'node': true,
-    'es6': true
+  env: {
+    node: true,
+    es6: true,
   },
   // this is the root project for all sub modules. stop searching for any
   // eslintrc files in parent directories.
-  'root': true,
-  'parserOptions': {
-    'sourceType': 'script',
+  root: true,
+  parserOptions: {
+    sourceType: 'script',
     // async/await support
-    'ecmaVersion': 8
+    ecmaVersion: 8,
   },
-  'plugins': [
+  plugins: [
     'header',
   ],
-  'extends': 'airbnb-base',
-  'rules': {
-    'strict': 0,
+  extends: 'airbnb-base',
+  rules: {
+    strict: 0,
 
     // allow dangling underscores for 'fields'
-    'no-underscore-dangle': ['error', {'allowAfterThis': true}],
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
 
     // enforce license header
     'header/header': [2, 'block', ['',
-      { pattern: ' * Copyright \\d{4} .*\\. All rights reserved\\.', template: ' * Copyright 2019 Adobe. All rights reserved.'},
+      { pattern: ' * Copyright \\d{4} .*\\. All rights reserved\\.', template: ' * Copyright 2019 Adobe. All rights reserved.' },
       ' * This file is licensed to you under the Apache License, Version 2.0 (the "License");',
       ' * you may not use this file except in compliance with the License. You may obtain a copy',
       ' * of the License at http://www.apache.org/licenses/LICENSE-2.0',
@@ -45,6 +45,6 @@ module.exports = {
       ' * OF ANY KIND, either express or implied. See the License for the specific language',
       ' * governing permissions and limitations under the License.',
       ' ',
-    ]]
-  }
+    ]],
+  },
 };
